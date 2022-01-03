cdece466ede64d60f79639f4686ebea98f5230c2
Patkalhrishikesh <patkalhrishikesh16@gmail.com>
2022-01-03 22:01:25 +0530

    DELETE

---
+++ my-app2/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <>
+      <MyComponent />
+    </>
+  );
+}
+
+function MyComponent() {
+  let [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    counter = counter + 1;
+
+    // RERENDER THE UI :: UPDATE THE INNERHTML ; innerHTML
+    setCounter(counter);
+  };
+
+  return (
+    <div>
+      <h1>Counter Application</h1>
+      <input type="button" value="Click Me" onClick={increment} />
+        <div>
+          <h1>Like {counter}</h1>
+        </div>
+
+    </div>
+  );
+}
--- readme.md
+++
@@ -1,3 +0,0 @@
-learning git hridhi
-
-dhsdndndcndjnad
